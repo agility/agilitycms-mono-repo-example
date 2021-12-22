4e82adc6a23f0668c56265324adb5e79dc2f0aee
jamesvidler <james.vidler@outlook.com>
2021-12-22 12:40:39 -0500

    added base tailwind config and theme overrides per project

--- agilitycms-nextjs-2/tailwind.config.js
+++ agilitycms-nextjs-2/tailwind.config.js
@@ -1,52 +1,18 @@
+// tailwind.config.js
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  presets: [
+    require('../tailwind-preset.js')
+  ],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@agility-mono-example/shared-components/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  // Customizations specific to this project would go here
   theme: {
-    container: {
-      screens: {
-        sm: "1200px",
-        md: "1200px",
-        lg: "1200px",
-        xl: "1200px",
-      },
-    },
-    fontFamily: {
-      display: ["Inter", "system-ui"],
-      sans: [
-        "system-ui",
-        "-apple-system",
-        "BlinkMacSystemFont",
-        '"Segoe UI"',
-        "Roboto",
-        '"Helvetica Neue"',
-        "Arial",
-        '"Noto Sans"',
-        "sans-serif",
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-      ],
-      serif: ["Georgia", "Cambria", '"Times New Roman"', "Times", "serif"],
-      mono: [
-        "Menlo",
-        "Monaco",
-        "Consolas",
-        '"Liberation Mono"',
-        '"Courier New"',
-        "monospace",
-      ],
-    },
     extend: {
-      width: {
-        15.1: "15.1rem",
-      },
       colors: {
-        transparent: "transparent",
-        agility: "#222",
-        gray: {
-          lightest: "#fff",
+        indigo: {
           100: "#f7fafc",
           200: "#edf2f7",
           300: "#e2e8f0",
@@ -55,35 +21,14 @@ module.exports = {
           600: "#718096",
           700: "#4a5568",
           800: "#2d3748",
-          900: "#1a202c",
-        },
-        primary: {
-          100: "#a273ff",
-          200: "#935bff",
-          300: "#8344ff",
-          400: "#742cff",
-          500: "#6415FF",
-          600: "#5a13e6",
-          700: "#5011cc",
-          800: "#460fb3",
-          900: "#3c0d99",
-        },
-        secondary: {
-          100: "#7c8ba1",
-          200: "#667892",
-          300: "#506582",
-          400: "#3a5173",
-          500: "#243E63",
-          600: "#203859",
-          700: "#1d324f",
-          800: "#192b45",
-          900: "#16253b",
-        },
-      },
-    },
+          900: "#1a202c"
+        }
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {
+      
+    },
   },
-  plugins: [require("@tailwindcss/typography")],
-};
+}
